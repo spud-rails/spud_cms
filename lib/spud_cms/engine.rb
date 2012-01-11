6351521ec3afb1757dd599b3d09d4cfe39520f20
David Estes <destes@redwindsw.com>
2012-01-11 17:50:15 -0500

    fixing precompiler directives

--- lib/spud_cms/engine.rb
+++ lib/spud_cms/engine.rb
@@ -7,5 +7,15 @@ require 'paperclip'
 module SpudCms
  class Engine < Rails::Engine
      engine_name :spud_cms
+     initializer :assets do |config| 
+    	Rails.application.config.assets.precompile += [ 
+    	   "jquery.wymeditor.pack.js",
+           "wymeditor/*", 
+           "wymeditor/lang/*", 
+           "wymeditor/skins/default/*", 
+           "wymeditor/skins/default/**/*",
+        ]
+  	end 
+     
  end
 end
