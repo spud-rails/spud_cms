90d54c627ee7ed0669209f7495bc5143d8a6637a
David Estes <destes@redwindsw.com>
2012-01-11 18:15:09 -0500

    updating js file

--- app/assets/javascripts/spud/admin/cms/application.js
+++ app/assets/javascripts/spud/admin/cms/application.js
@@ -3,7 +3,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function() {
+$(window).load(function() {
        $('textarea.wysiwym').wymeditor({
          
        });
