e4a30b8d3a31daf402790b6e45ede67d82402ff9
David Estes <destes@redwindsw.com>
2012-01-11 18:01:40 -0500

    changes

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-module SpudCms
+module SpudTest
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -44,6 +44,5 @@ module SpudCms
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile += ['wymeditor/*']
   end
 end
