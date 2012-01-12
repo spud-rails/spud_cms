bfc3d806e25d5865c2b50142dc20602ffe459c92
David Estes <destes@redwindsw.com>
2012-01-12 18:57:43 -0500

    added validations to templates

--- app/models/spud_template.rb
+++ app/models/spud_template.rb
@@ -1,3 +1,7 @@
 class SpudTemplate < ActiveRecord::Base
 	has_many :spud_pages,:dependent => :nullify
+
+	validates :base_layout, :presence => true
+	validates :page_parts, :presence => true
+	validates :name, :presence => true,:uniqueness => true
 end
