ee05b275286e421ebf1a73eda4c48b92d74ab744
David Estes <davydotcom@gmail.com>
2012-01-23 16:18:38 -0500

    added default_page_parts option to cms

--- lib/spud_cms/configuration.rb
+++ lib/spud_cms/configuration.rb
@@ -2,11 +2,12 @@ module Spud
   module Cms
     include ActiveSupport::Configurable
 
-    config_accessor :menus_enabled,:templates_enabled,:root_page_name
+    config_accessor :menus_enabled,:templates_enabled,:root_page_name,:default_page_parts
 
     self.root_page_name = "home"
     self.menus_enabled = true
     self.templates_enabled = false
+    self.default_page_parts = ["Body"]
     
   end
 end
