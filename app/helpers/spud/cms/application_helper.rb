e13225a89e4f8daa7c6c689f17d248411084e020
David Estes <destes@redwindsw.com>
2012-01-26 20:09:40 -0500

    improving menu listing performance

--- app/helpers/spud/cms/application_helper.rb
+++ app/helpers/spud/cms/application_helper.rb
@@ -26,6 +26,7 @@ module Spud::Cms::ApplicationHelper
 
 
 	def sp_list_menu(options = {})
+
 		menu = SpudMenu
 		if !options.blank?
 			if options.has_key?(:menu_id)
@@ -46,11 +47,15 @@ module Spud::Cms::ApplicationHelper
 		if menu.blank?
 			return ""
 		end
-		menu_items = menu.spud_menu_items_combined.select("spud_menu_items.id as id,spud_menu_items.url as url,spud_menu_items.parent_type as parent_type,spud_menu_items.menu_order as menu_order,spud_menu_items.parent_id as parent_id,spud_menu_items.name as name,spud_pages.url_name as url_name").order(:parent_type,:parent_id).joins("LEFT JOIN spud_pages ON (spud_pages.id = spud_menu_items.spud_page_id)")
-		# grouped_items = menu_items.group_by(&:parent_type,&:parent_id)
-		menu_items.select{|item| item.parent_type == 'SpudMenu'}.sort_by{|p| p.menu_order}.each do |item|
+		menu_items = menu.spud_menu_items_combined.select("spud_menu_items.id as id,spud_menu_items.url as url,spud_menu_items.parent_type as parent_type,spud_menu_items.menu_order as menu_order,spud_menu_items.parent_id as parent_id,spud_menu_items.name as name,spud_pages.url_name as url_name").order(:parent_type,:parent_id).joins("LEFT JOIN spud_pages ON (spud_pages.id = spud_menu_items.spud_page_id)").all
+		grouped_items = menu_items.group_by(&:parent_type)
+		
+		child_items = grouped_items["SpudMenuItem"].group_by(&:parent_id)
+		
+
+		grouped_items["SpudMenu"].sort_by{|p| p.menu_order}.each do |item|
 			content += "<li><a href='#{!item.url_name.blank? ? page_path(:id => item.url_name) : item.url}'>#{item.name}</a>"
-			content += sp_list_menu_item(menu_items,item.id)
+			content += sp_list_menu_item(child_items,item.id)
 			content += "</li>"
 		end
 		# menu.spud_menu_items.order(:menu_order).each do |item|
@@ -63,11 +68,13 @@ module Spud::Cms::ApplicationHelper
 	end
 private
 	def sp_list_menu_item(items,item_id)
-		spud_menu_items = items.select{|item| item.parent_type == 'SpudMenuItem' && item.parent_id == item_id}
-		if spud_menu_items.count == 0
+
+		spud_menu_items = items[item_id]
+		if spud_menu_items == nil
 			return ""
 		end
 		content = "<ul>"
+		
 		spud_menu_items.sort_by{|p| p.menu_order}.each do |item|
 			content += "<li><a href='#{!item.url_name.blank? ? page_path(:id => item.url_name) : item.url}'>#{item.name}</a>"
 			content += sp_list_menu_item(items,item.id)
