0f541ca8f1ed6e8f8178a7a40a670113c18884fe
David Estes <destes@redwindsw.com>
2012-01-26 22:03:20 -0500

    fixed bug in sp_list_menus

--- app/helpers/spud/cms/application_helper.rb
+++ app/helpers/spud/cms/application_helper.rb
@@ -48,9 +48,12 @@ module Spud::Cms::ApplicationHelper
 			return ""
 		end
 		menu_items = menu.spud_menu_items_combined.select("spud_menu_items.id as id,spud_menu_items.url as url,spud_menu_items.parent_type as parent_type,spud_menu_items.menu_order as menu_order,spud_menu_items.parent_id as parent_id,spud_menu_items.name as name,spud_pages.url_name as url_name").order(:parent_type,:parent_id).joins("LEFT JOIN spud_pages ON (spud_pages.id = spud_menu_items.spud_page_id)").all
+
 		grouped_items = menu_items.group_by(&:parent_type)
-		
-		child_items = grouped_items["SpudMenuItem"].group_by(&:parent_id)
+		if grouped_items["SpudMenu"].blank?
+			return ""
+		end
+		child_items = grouped_items["SpudMenuItem"].blank? ? [] : grouped_items["SpudMenuItem"].group_by(&:parent_id)
 		
 
 		grouped_items["SpudMenu"].sort_by{|p| p.menu_order}.each do |item|
