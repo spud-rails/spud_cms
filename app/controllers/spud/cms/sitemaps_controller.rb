c1ad42162ab69c401b0a4424f7e981a691cccbb5
David Estes <destes@redwindsw.com>
2012-04-03 16:47:29 -0400

    fixed bug in cache clear button

--- app/controllers/spud/cms/sitemaps_controller.rb
+++ app/controllers/spud/cms/sitemaps_controller.rb
@@ -3,6 +3,11 @@ class Spud::Cms::SitemapsController < Spud::ApplicationController
 	caches_page :show,:expires_in => 1.day
 	def show
 		@pages = SpudPage.published_pages.public.order(:spud_page_id)
+		if Spud::Core.multisite_mode_enabled
+			site_config = Spud::Core.site_config_for_host(request.host_with_port)
+			@pages = @pages.site(!site_config.blank? ? site_config[:site_id] : nil) 
+		end
+		@pages = @pages.all
 		respond_with @pages
 	end
 end
