4a9ac7a72fd3c8a20b507f4f1105e06e87c40610
David Estes <destes@redwindsw.com>
2012-05-10 16:28:11 -0400

    added revision maintainability to spud cms

--- app/models/spud_page_partial.rb
+++ app/models/spud_page_partial.rb
@@ -1,4 +1,22 @@
 class SpudPagePartial < ActiveRecord::Base
 	belongs_to :spud_page
 	attr_accessible :name,:spud_page_id,:content,:format
+	before_save :maintain_revisions
+
+
+	def maintain_revisions
+		if !self.changed.include?('content')
+			return true
+		end
+		revision = SpudPagePartialRevision.new(:spud_page_id => self.spud_page_id,:name => self.name,:format => self.format,:content => self.content)
+			revision.save
+			if Spud::Cms.max_revisions > 0
+				revision_count = SpudPagePartialRevision.where(:spud_page_id => self.spud_page_id,:name => self.name).count
+				if revision_count > Spud::Cms.max_revisions
+					revision_bye = SpudPagePartialRevision.where(:spud_page_id => self.spud_page_id,:name => self.name).order("created_at ASC").first
+					revision_bye.destroy if !revision_bye.blank?
+				end
+			end
+		return true
+	end
 end
--- lib/spud_cms/configuration.rb
+++ lib/spud_cms/configuration.rb
@@ -2,7 +2,7 @@ module Spud
   module Cms
     include ActiveSupport::Configurable
 
-    config_accessor :menus_enabled,:templates_enabled,:root_page_name,:default_page_parts,:yield_body_as_content_block,:default_page_layout,:enable_sitemap,:enable_full_page_caching,:enable_action_caching,:multisite_config
+    config_accessor :menus_enabled,:templates_enabled,:root_page_name,:default_page_parts,:yield_body_as_content_block,:default_page_layout,:enable_sitemap,:enable_full_page_caching,:enable_action_caching,:multisite_config,:max_revisions
 
     self.root_page_name = "home"
     self.menus_enabled = true
@@ -13,6 +13,7 @@ module Spud
     self.enable_full_page_caching = false
     self.enable_action_caching = false
     self.enable_sitemap = true
+    self.max_revisions = 10
     self.multisite_config = []
     def self.site_config_for_short_name(short_name)
         configs = Spud::Cms.multisite_config.select{|p| p[:short_name].to_s == short_name.to_s}
