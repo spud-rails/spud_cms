0bc762831bb058c58db41c7627ea5748205e2949
David Estes <davydotcom@gmail.com>
2014-01-06 11:16:31 -0500

    working on fixing layout change mechanism

--- app/assets/javascripts/spud/admin/cms/pages.js
+++ app/assets/javascripts/spud/admin/cms/pages.js
@@ -18,12 +18,20 @@ spud.admin.cms.pages = {};
     var tabButtons = $('.formtabs .formtab_buttons').first();
     for(var x=0;x<tabNames.length;x++)
     {
-      var tabButton = $('<li><a href="#tab-' + x + '" data-toggle="tab">' + tabNames[x] + '</a></li>');
+      var tabButton = $('<li><a class="spud-page-tab-button" href="#tab-' + x + '" data-toggle="tab">' + tabNames[x] + '</a></li>');
       if(x === 0) {
         tabButton.addClass('active');
       }
       tabButtons.append(tabButton);
-    }
 
+    }
+    pages.tabMonitor();
+  };
+  pages.tabMonitor = function() {
+    $('a.spud-page-tab-button[data-toggle="tab"]').on('shown', function (e) {
+      var editors = $(e.target).find('.spud-formatted-editor');
+      spud.admin.editor.unload('.formtab [code-mirror-id]')
+      spud.admin.editor.init(editors);
+    })
   };
 })();
